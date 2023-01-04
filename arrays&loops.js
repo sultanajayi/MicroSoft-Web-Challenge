0cf2b46afadd588832b5986c5786868ef1bfd17b
sultanajayi <hayobamidele39@gmail.com>
2023-01-04 07:19:38 +0100

    Arrays&Loops

---
+++ arrays&loops.js
@@ -0,0 +1,38 @@
+let myArray = [];
+
+let iceCreamFlavors = ["Chocolate", "Strawberry", "Vanilla", "Pistachio", "Rocky Road"];
+
+//iceCreamFlavors[4] = "Butter Pecan"; //Changed "Rocky Road" to "Butter Pecan"
+
+//iceCreamFlavors[5] = "Cookie Dough"; //Added "Cookie Dough"
+
+// Counting up to 10
+/** for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }**/
+
+
+  //Counting up to 10
+/** let i = 0;
+while (i < 10) {
+ console.log(i);
+ i++;
+}**/
+
+
+
+/**for (let i = 0; i < iceCreamFlavors.length; i++) {
+  console.log(iceCreamFlavors[i]);
+} **/
+
+
+for (let i = 1; i < 20; i++) {
+    if (i % 2 === 1) {
+        console.log(i)
+    }
+    
+}
+
+
+//Ends when all flavors are printed
+//console.log(iceCreamFlavors.length);
